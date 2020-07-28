5762814375fa20b89c445bfefc36fb736ddd7138
Luis Saavedra <nriqu322@gmail.com>
2020-07-28 17:25:16 -0600

    write test for capitalize, reverseSting and calculator

--- src/reverseString.js
+++ src/reverseString.js
@@ -1,3 +1,8 @@
-const reverseString = (string) => string.split('').reverse().join('');
+const reverseString = (string) => {
+  if (typeof string !== 'string' || string.length === 0) {
+    return '';
+  }
+  return string.split('').reverse().join('');
+};
 
 export default reverseString;
